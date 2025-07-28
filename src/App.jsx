37d04e557e20e656da8f601d8634607192cabfa8
kouuch <rifkyirsada2@gmail.com>
2025-07-28 23:19:30 +0800

    instal tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Article from "./components/Article";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Article />
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className=''>
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar

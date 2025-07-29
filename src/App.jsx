e460e91654993f16dbc1352134b3cfa07386c046
kouuch <rifkyirsada2@gmail.com>
2025-07-29 19:03:25 +0800

    import halaman homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import "./App.css";
-import Article from "./components/Article";
-import Blog from "./components/Blog";
+// import Article from "./components/Article";
+// import Blog from "./components/Blog";
+import Home from "./pages/Homepage";
+
 function App() {
   return (
-    <>
-      {/* props gunakan {} untuk data aray atau bukan string*/}
-      <Article
-        name="budi"
-        title={["ini adalah satu", "ini adalah dua", "ini adalah tiga"]}
-      />
-      <br />
-      <Article
-        name="Tono"
-        title={["ini adalah tiga", "ini adalah dua", "ini adalah satu"]}
-      />
-      <br />
-      <Blog />
-    </>
+    // <>
+    //   {/* props gunakan {} untuk data aray atau bukan string*/}
+    //   <Article
+    //     name="budi"
+    //     title={["ini adalah satu", "ini adalah dua", "ini adalah tiga"]}
+    //   />
+    //   <br />
+    //   <Article
+    //     name="Tono"
+    //     title={["ini adalah tiga", "ini adalah dua", "ini adalah satu"]}
+    //   />
+    //   <br />
+    //   <Blog />
+    // </>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>
+    <h1>ini halaman Homepage</h1>
+    </>
+  )
+}
+
+export default Homepage

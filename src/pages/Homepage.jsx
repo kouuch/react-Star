0631ae7859020e7c05c2176377d48ca454cf3018
kouuch <rifkyirsada2@gmail.com>
2025-07-31 13:25:24 +0800

    penggunaan pada umumnya

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
 import post from '../post.json'
 import Article from '../components/Article'
 
 function Homepage(){
+  let search = ''
+  
+  const changeSearch = (e) =>{
+    console.log(e.target.value)
+    search = e.target.value
+  }
   return (
     <>
     <h1>ini halaman Homepage</h1>
+    <br />
+    <div>
+      Cari Artikel : <input onChange={changeSearch} />
+    </div>
+    <small>0 data dengan pencarian kata {search}</small>
+    <br />
+    <br />
     {post.map(({id, title, tags, date}) => (
       // exmple 1
       // <Article key={id} title = {title} tags={tags} date={date} />

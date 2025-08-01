aa46ec49d35183c23660b4bfd1884bc74ffad7c3
kouuch <rifkyirsada2@gmail.com>
2025-08-01 21:35:11 +0800

    halaman about

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      halaman About
+    </div>
+  )
+}
+
+export default About
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Homepage";
+import About from "../pages/About";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/About",
+    element: <About />,
   },
 ]);
